09e3b3057af864cef837e2eb934b76fbc384004c
Daniela Sousa <danielasousa.info@gmail.com>
2024-04-19 15:43:12 +0100

    Invocação do método move() e imprimir Heatlh e Cost

---
+++ GameUnits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GameUnits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SettlerUnit settler = new SettlerUnit();
+            MilitaryUnit military = new MilitaryUnit(10);
+
+            settler.Move(5);
+            military.Move(3);
+
+            Console.WriteLine($"Settler Health: {settler.Health}");
+            Console.WriteLine($"Settler Cost: {settler.Cost}");
+            Console.WriteLine($"Military Health: {military.Health}");
+            Console.WriteLine($"Military Cost: {military.Cost}");
+        }
+    }
+}
